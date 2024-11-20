1adf35ff06c75a6b20264a4844dec8e482db750d
fury1231 <61882422+fury1231@users.noreply.github.com>
2024-11-20 20:10:19 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,16 +1,13 @@
 const sqlite3 = require('sqlite3').verbose();
-
-const db = new sqlite3.Database('./irc_users.db', (err)=>{
-    if(err) {
-        console.error('無法連接到資料庫:', err.message);
-    } else {
-        console.log('成功連接到資料庫');
-    }
+const db = new sqlite3.Database('./irc_users.db', (err) => {
+	if (err) {
+		console.error('無法連接到資料庫:', err.message);
+	} else {
+		console.log('成功連接到資料庫');
+	}
 });
-
-db.serialize(()=>{
-    db.run(
-        `
+db.serialize(() => {
+	db.run(`
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             nickname TEXT NOT NULL,
@@ -18,12 +15,11 @@ db.serialize(()=>{
             socket_id TEXT NOT NULL,
             joined_at DATETIME DEFAULT CURRENT_TIMESTAMP
         )
-    `
-    , (err) => {
-        if(err) {
-            console.error('無法創建資料表:', err.message);
-        } else {
-            console.log('資料表初始化成功');
-        }
-    })
+    `, (err) => {
+		if (err) {
+			console.error('無法創建資料表:', err.message);
+		} else {
+			console.log('資料表初始化成功');
+		}
+	})
 })
